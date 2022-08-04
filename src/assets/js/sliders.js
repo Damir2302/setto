2fc5bdc61ea7cc10d3215cdd0ff4368b68174697
Damir2302 <damir23.02@mail.ru>
2022-08-04 17:55:22 +0300

    product page

--- src/assets/js/sliders.js
+++ src/assets/js/sliders.js
@@ -1,41 +1,43 @@
 $(document).ready(function() {
 
     let solutionsSlider = new Swiper(".solutions-slider", {
-        slidesPerView: 1,
+      slidesPerView: 1,
 
-        breakpoints: {
-          744: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-
-          1024: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-          },
-
-          1280: {
-            slidesPerView: 2,
-            spaceBetween: 40,
-          }
+      breakpoints: {
+        744: {
+          slidesPerView: 2,
+          spaceBetween: 20,
         },
 
-        navigation: {
-          nextEl: '.solutions .slider-button-next',
-          prevEl: '.solutions .slider-button-prev',
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+
+        1280: {
+          slidesPerView: 2,
+          spaceBetween: 40,
         }
-    })
+      },
 
-    let catTabSlider;
-    let init;
+      navigation: {
+        nextEl: '.solutions .slider-button-next',
+        prevEl: '.solutions .slider-button-prev',
+      }
+  })
 
-    catSliderCheck();
+  let catTabSlider;
+  let init;
 
-    $(window).on('resize', function() {
-      catSliderCheck()
-    })
+  catSliderCheck();
 
-    function catSliderCheck() {
+  $(window).on('resize', function() {
+    catSliderCheck()
+  })
+
+  function catSliderCheck() {
+
+    if ($('.cat-tab-slider').length) {
 
       if ($(window).width() < 744) {
         if (!init) {
@@ -51,67 +53,119 @@ $(document).ready(function() {
           catTabSlider.destroy();
         }
       }
+
     }
+  }
 
-    let catSlider = new Swiper(".cat-slider", {
+  let catSlider = new Swiper(".cat-slider", {
+    slidesPerView: 1,
+
+    breakpoints: {
+      744: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      }
+    },
+
+    navigation: {
+      nextEl: '.cat .slider-button-next',
+      prevEl: '.cat .slider-button-prev',
+    }
+  })
+
+  /////////////////////////////////////////////////////////////////////
+  // КАРТОЧКА ТОВАРА
+  ////////////////////////////////////////////////////////////////////
+  let item_cart_pagination;
+
+  item_cart_pagination = new Swiper('.product__slider-pagination .swiper', {
+      slidesPerView: 'auto',
+      watchSlidesProgress: true,
+      direction: 'vertical'
+  });
+
+  let item_cart;
+
+  item_cart = new Swiper('.product__slider', {
+      slidesPerView: "auto",
+      centeredSlides: true,
       slidesPerView: 1,
-
-      breakpoints: {
-        744: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        }
+      spaceBetween: 30,
+      thumbs: {
+        swiper: item_cart_pagination,
       },
 
       navigation: {
-        nextEl: '.cat .slider-button-next',
-        prevEl: '.cat .slider-button-prev',
+        nextEl: '.product__item .slider-button-next',
+        prevEl: '.product__item .slider-button-prev',
       }
+  });
+
+  let productTabSlider;
+  let sliderInit;
+
+  prodTabCheck();
+
+  $(window).on('resize', function() {
+    prodTabCheck()
   })
 
-    /////////////////////////////////////////////////////////////////////
-    // КАРТОЧКА ТОВАРА
-    ////////////////////////////////////////////////////////////////////
-    let item_cart_pagination;
+  function prodTabCheck() {
 
-    item_cart_pagination = new Swiper('.product__slider-pagination', {
-        slidesPerView: 'auto',
-        spaceBetween: 10,
-        watchSlidesProgress: true,
-        direction: 'horizontal',
+    if ($('.tabs-slide').length) {
 
-        breakpoints: {
-            768: {
-                direction: 'vertical'
-            }
+      if ($(window).width() < 744) {
+        if (!sliderInit) {
+          sliderInit = true;
+          productTabSlider = new Swiper(".tabs-slide", {
+            slidesPerView: 'auto',
+            freeMode: true,
+          })
         }
-    });
+      } else {
+        if (typeof(productTabSlider) !== "undefined" ) {
+          sliderInit = false;
+          productTabSlider.destroy();
+        }
+      }
 
-    let item_cart;
+    }
+  }
 
-    item_cart = new Swiper('.product__slider', {
-        slidesPerView: "auto",
-        centeredSlides: true,
-        slidesPerView: 1,
+  let interiorSlider = new Swiper(".interior__slider .swiper", {
+    slidesPerView: 1,
+
+    breakpoints: {
+      744: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      1024: {
+        slidesPerView: 2,
         spaceBetween: 30,
-        thumbs: {
-          swiper: item_cart_pagination,
-        },
+      },
 
-        navigation: {
-          nextEl: '.product__item .slider-button-next',
-          prevEl: '.product__item .slider-button-prev',
-        }
-    });
+      1280: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      }
+    },
+
+    navigation: {
+      nextEl: '.interior__slider .slider-button-next',
+      prevEl: '.interior__slider .slider-button-prev',
+    }
+  })
 
 })
